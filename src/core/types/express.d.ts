b397a14b56ac2249d0badf2652e39ddaf18d9da3
danmachooo <johnpauldanmachi@gmail.com>
2026-03-11 14:27:07 +0800

    Updated and added role

--- src/core/types/express.d.ts
+++ src/core/types/express.d.ts
@@ -1,10 +1,14 @@
+import { Role } from '@prisma/client'
 import 'express'
 
 declare global {
   namespace Express {
     interface Request {
       user: {
-        userId
+        id: string
+        email: string
+        name: string,
+        role: Role
       }
       validatedBody: any
       validatedParams: any
