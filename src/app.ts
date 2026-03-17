3b8122ca8ad45f30183f55cf17835e006111a74a
danmachooo <johnpauldanmachi@gmail.com>
2026-03-17 16:27:46 +0800

    Added health and cors

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,30 @@ import "dotenv/config"
 
 import { errorHandler } from '@/core/middleware/error-handler'
 import { requestLogger } from '@/core/middleware/request-logger'
+import { sendResponse } from '@/core/utils/response'
 import { apiRouter } from '@/routes'
+import { appConfig } from '@/config/config'
 
 const app = express()
 
+const frontendUrl = appConfig.frontend.url
+const backendUrl = appConfig.app.url
+
 app.use(helmet())
 app.use(cors({
-    origin: ['http://localhost:8000'],
+    origin: [frontendUrl, backendUrl],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization "]
 }))
 app.use(express.json())
 app.use(requestLogger)
+
+app.get('/health', (_req, res) => {
+  return sendResponse(res, 200, 'Service is healthy.', {
+    status: 'ok'
+  })
+})
 
 app.use('/api', apiRouter)
 
